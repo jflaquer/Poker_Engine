53bca577c964b459c14f3e21a86804c8849fc091
w1zgam3r <jflaquer@udel.edu>
2015-09-28 15:39:24 -0400

    Updated Hand Evaluation in DeckTest Class

--- SpringBorgata/src/main/java/pokerBase/DeckTest.java
+++ SpringBorgata/src/main/java/pokerBase/DeckTest.java
@@ -2,7 +2,12 @@ package pokerBase;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
+
+import pokerEnums.eRank;
+import pokerEnums.eSuit;
 
 public class DeckTest {
 
@@ -10,6 +15,23 @@ public class DeckTest {
 	public void test() {
 		Deck myTest = new Deck ();
 		assertTrue (myTest.getTotalCards () == 52);
+	}
+	
+	public void testHand() {
+		Card myCard1 = new Card(eSuit.SPADES, eRank.ACE, 0);
+		Card myCard2 = new Card(eSuit.DIAMONDS, eRank.KING, 1);
+		Card myCard3 = new Card(eSuit.CLUBS, eRank.QUEEN, 2);
+		Card myCard4 = new Card(eSuit.HEARTS, eRank.JACK, 3);
+		Card myCard5 = new Card(eSuit.SPADES, eRank.TEN, 4);
+		Hand myHand = new Hand();
+		myHand.AddCardToHand(myCard1);
+		myHand.AddCardToHand(myCard2);
+		myHand.AddCardToHand(myCard3);
+		myHand.AddCardToHand(myCard4);
+		myHand.AddCardToHand(myCard5);
+		ArrayList<Card> testHand = myHand.getCards();
+		ArrayList<Card> straight = myHand.getBestHand();
+		assertTrue(testHand==straight);
 	}
 
 }
